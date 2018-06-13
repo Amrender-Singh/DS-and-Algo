c21e1a5828a1c092caeabadd97a029ae750fdb83
Amrender-Singh <amrendersingh1995@gmail.com>
2018-06-13 23:27:08 +0530

    594 One little

---
+++ Data structures/Stl/OnelitteEndians.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+class Reader{
+    BufferedReader br;
+    StringTokenizer st;
+    Reader(){
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+    String next(){
+        if(st == null || !st.hasMoreElements()){
+            try {
+                st = new StringTokenizer(br.readLine());                
+            } catch (Exception e) {
+               
+            }
+        }
+        return st.nextToken();
+    }
+    int nextInt(){
+        return Integer.parseInt(next());
+    }
+    double nextDouble(){
+        return Double.parseDouble(next());
+    }
+    long nextLong()
+    {
+        return Long.parseLong(next());
+    }
+    String nextLine()
+    {
+        String str = "";
+        try
+        {
+            str = br.readLine();
+        }
+        catch (IOException e)
+        {
+            
+        }
+        return str;
+    }
+}
+
+ public class OnelitteEndians {
+    public static void main(String[] args) {
+        Reader sc =  new Reader();
+        String s;
+        while((s = sc.nextLine()) != null){
+            System.out.println(s+" converts to "+Integer.reverseBytes(Integer.parseInt(s)));
+        }
+    }
+}
