c2056318b094a6da31f8212567a161b49171d569
Amrender-Singh <amrendersingh1995@gmail.com>
2018-08-20 23:41:23 +0530

    11308 - Bankrupt Baker

---
+++ Data structures/TreeMap/BankruptBaker.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.util.*;
+import java.text.DecimalFormat;
+
+class Reader{
+    BufferedReader br;
+    StringTokenizer st;
+    Reader(){
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+    String next(){
+        if(st == null || !st.hasMoreElements()){
+            try {
+                st = new StringTokenizer(br.readLine());                
+            } catch (Exception e) {
+               return null;
+            }
+        }
+        return st.nextToken();
+    }
+    int nextInt(){
+        return Integer.parseInt(next());
+    }
+    double nextDouble(){
+        return Double.parseDouble(next());
+    }
+    long nextLong()
+    {
+        return Long.parseLong(next());
+    }
+    String nextLine()
+    {
+        String str = "";
+        try
+        {
+            str = br.readLine();
+        }
+        catch (IOException e)
+        {
+            
+        }
+        return str;
+    }
+}
+class RecipeMap{
+    String recipeName;
+    int cost;
+    RecipeMap(String recipeName, int cost){
+        this.recipeName = recipeName;
+        this.cost = cost;
+    }
+}
+public class BankruptBaker{
+    
+    public static void readInputAndCreateMap(Reader sc, int m, int n, ArrayList<RecipeMap> recipesPriceMap){
+        HashMap<String, Integer> ingredientPriceMap = new HashMap<>();
+        while(m --> 0){
+            ingredientPriceMap.put(sc.next().trim(), sc.nextInt());
+        }
+        while(n --> 0){
+            String recipeName = sc.nextLine();
+            int k = sc.nextInt();
+            int cost = 0;
+            while(k --> 0){
+                Integer price = ingredientPriceMap.get(sc.next().trim());
+                int units = sc.nextInt();
+                if(price != null)
+                    cost += (price*units);
+            }
+            recipesPriceMap.add(new RecipeMap(recipeName, cost));
+        }
+    }
+    public static void main(String[] args) {
+        Reader sc = new Reader();
+        StringBuilder output = new StringBuilder();
+        int noOfTestcases = sc.nextInt();
+        while(noOfTestcases --> 0){
+            ArrayList<RecipeMap> recipesPriceMap = new ArrayList<>();
+            String binder = sc.nextLine();
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            int b = sc.nextInt();
+            output.append(binder.toUpperCase() + "\n");
+            boolean foundOne = false;
+            readInputAndCreateMap(sc, m, n, recipesPriceMap);
+            Collections.sort(recipesPriceMap, (x, y)->{
+                if(x.cost != y.cost)
+                    return x.cost - y.cost;
+                return x.recipeName.compareTo(y.recipeName);
+            });
+            for(RecipeMap r : recipesPriceMap){
+                if(r.cost > b){
+                    break;
+                } 
+                output.append(r.recipeName + "\n");
+                foundOne = true;
+            }
+            if(!foundOne)
+                output.append("Too expensive!\n");
+            output.append("\n");
+        }
+        System.out.print(output.toString());
+    }
+}
