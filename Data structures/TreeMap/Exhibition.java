6fa5e9b5158f8e12db925116d6d6d3bfc1c5ab82
Amrender-Singh <amrendersingh1995@gmail.com>
2018-08-21 23:23:49 +0530

    11348 - Exhibition

---
+++ Data structures/TreeMap/Exhibition.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+
+class Reader{
+    BufferedReader br;
+    StringTokenizer st;
+    Reader(){
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+    String next(){
+        if(st == null || !st.hasMoreElements()){
+            try {
+                st = new StringTokenizer(br.readLine());                
+            } catch (Exception e) {
+               return null;
+            }
+        }
+        return st.nextToken();
+    }
+    int nextInt(){
+        return Integer.parseInt(next());
+    }
+    double nextDouble(){
+        return Double.parseDouble(next());
+    }
+    long nextLong()
+    {
+        return Long.parseLong(next());
+    }
+    String nextLine()
+    {
+        String str = "";
+        try
+        {
+            str = br.readLine();
+        }
+        catch (IOException e)
+        {
+            
+        }
+        return str;
+    }
+}
+
+public class Exhibition{
+    
+    public static int readInputAndCreateMap(Reader sc, LinkedHashMap<Integer, Integer> list){
+        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
+        int noOfFriends = sc.nextInt();
+        int i = 1;
+        while(i <= noOfFriends){
+            int m = sc.nextInt();
+            while(m --> 0){
+                int stamp = sc.nextInt();
+                HashSet<Integer> set = map.get(stamp);
+                if(set == null)
+                    set = new HashSet<>();
+                set.add(i);
+                map.put(stamp, set);
+            }
+            list.put(i, 0);
+            i++;
+        }
+        int noOfUniqueStamps = 0;
+        for(Map.Entry<Integer, HashSet<Integer>> entry : map.entrySet()){
+            if(entry.getValue().size() == 1){
+                for(Integer j : entry.getValue()){
+                    Integer count = list.get(j);
+                    list.put(j, count+1);
+                }
+                noOfUniqueStamps++;
+            }
+        }
+        return noOfUniqueStamps;
+    }
+    public static void main(String[] args) {
+        Reader sc = new Reader();
+        StringBuilder output = new StringBuilder();
+        int nofOfTestCases = sc.nextInt();
+        int i = 1;
+        while(i <= nofOfTestCases){
+            LinkedHashMap<Integer, Integer> list = new LinkedHashMap<>();
+            int noOfUniqueStamps = readInputAndCreateMap(sc, list);
+            System.out.print("Case "+ i + ":");
+            for(Map.Entry<Integer,Integer> entry : list.entrySet()){
+                double percentage = (double)(entry.getValue()*100)/noOfUniqueStamps;
+                System.out.printf(" %.6f",percentage);
+                System.out.print("%");
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
