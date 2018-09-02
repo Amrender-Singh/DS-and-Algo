9ad2023d8d9ee3fe8c415a70ad9fc5a721a3f29c
Amrender-Singh <amrendersingh1995@gmail.com>
2018-09-02 16:17:29 +0530

    12049 - Just Prune The List

--- Data structures/TreeMap/JustPruneTheList.java
+++ Data structures/TreeMap/JustPruneTheList.java
@@ -44,11 +44,12 @@ class Reader{
 }
 
 public class JustPruneTheList{
-    public static void readInputAndCreateMap(Reader sc, int n, HashMap<Integer,Integer> map){
+    public static void readInputAndCreateMap(Reader sc, int n, HashMap<Integer,Integer> map, boolean inc){
         int a;
+        int add = inc ? 1 : -1;
         while(n --> 0){
             a = sc.nextInt();
-            map.put(a, map.getOrDefault(a,0)+1);
+            map.put(a, map.getOrDefault(a,0)+add);
         }
     }
     public static void main(String[] args) {
@@ -56,30 +57,16 @@ public class JustPruneTheList{
         StringBuilder output = new StringBuilder();
         int noOfTestCases = sc.nextInt();
         int count, m, n;
-        HashMap<Integer,Integer> list1, list2;
+        HashMap<Integer,Integer> list1;
         while(noOfTestCases --> 0){
             count = 0;
             m = sc.nextInt();
             n = sc.nextInt();
             list1 = new HashMap<Integer,Integer>();
-            list2 = new HashMap<Integer,Integer>();
-            readInputAndCreateMap(sc, m, list1);
-            readInputAndCreateMap(sc, n, list2);
-            Integer list2Val;
+            readInputAndCreateMap(sc, m, list1, true);
+            readInputAndCreateMap(sc, n, list1, false);
             for(Map.Entry<Integer, Integer> entry : list1.entrySet()){
-                list2Val = list2.get(entry.getKey());
-                if(list2Val == null)
-                    count += entry.getValue();
-                else{
-                    count += (Math.abs(entry.getValue() - list2Val));
-                    list2.remove(entry.getKey());
-                }
-            }
-            Integer list1Val;
-            for(Map.Entry<Integer, Integer> entry : list2.entrySet()){
-                list1Val = list1.get(entry.getKey());
-                if(list1Val == null)
-                    count += entry.getValue();
+                    count += Math.abs(entry.getValue());
             }
             output.append(count+"\n");
         }
